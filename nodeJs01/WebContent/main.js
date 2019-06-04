6ae5926864656e0d9753c4c94ccb8c47fe9f54cf
coffeemori92 <coffeemori92@naver.com>
2019-06-04 16:29:09 +0900

    1

---
+++ nodeJs01/WebContent/main.js
@@ -0,0 +1,20 @@
+var http = require("http");
+var fs = require("fs");
+var url = require('url');
+
+var app = http.createServer(function(req, res){
+	
+	var _url = req.url;
+	var queryData = url.parse(_url, true).query;
+	console.log(queryData.id);
+	
+	if(_url == "/"){
+		_url = "/index.html";
+	}
+	if(_url == "/favicon.ico"){
+		return res.writeHead(404);
+	}
+	res.writeHead(200);
+	res.end(queryData.id);
+});
+app.listen(3000);
